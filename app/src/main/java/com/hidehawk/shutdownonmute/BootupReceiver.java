efba8df77de9fb20bf13d2b0a64a1d16b404591d
Martin Jonik <jonik@lga.de>
2016-10-11 15:15:38 +0200

    screenshot added

--- app/src/main/java/com/hidehawk/shutdownonmute/BootupReceiver.java
+++ app/src/main/java/com/hidehawk/shutdownonmute/BootupReceiver.java
@@ -3,16 +3,21 @@ package com.hidehawk.shutdownonmute;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 public class BootupReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        // register AlarmReceiver after Bootup
+        // register AlarmReceiver after Bootup, if application is enabled
 
-        Log.i(BootupReceiver.class.getName(), "onReceive(): BOOT_COMPLETED");
+        SharedPreferences prefs = context.getSharedPreferences(Config.SHARED_PREFS_FILE, Context.MODE_PRIVATE);
+        boolean enabled = prefs.getBoolean(Config.PREF_ENABLED, Config.DEFAULT_ENABLED);
 
-        AlarmReceiver.register(context);
+        Log.i(BootupReceiver.class.getName(), "onReceive(): BOOT_COMPLETED, enabled=" + enabled);
+
+        if (enabled)
+            AlarmReceiver.register(context);
     }
 }
